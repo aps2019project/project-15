5eccfa1844b3708b17a608acca0bce6a594c13f4
ghshenavar <ghshenavar@gmail.com>
2019-05-04 11:12:35 +0430

    battle class deleted

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,6 +1,7 @@
 package model;
 
 import controller.Controller;
+import view.View;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,8 @@ public class Game {
     private int turn = (int) (Math.random() % 2 + 1);
     private int result;
     private int timeOfGame;
+
+    private View view = View.getInstance();
 
     public Map getMap() {
         return map;
@@ -151,6 +154,102 @@ public class Game {
         else if (this.turn == 2) {
             this.turn = 1;
         }
+    }
+
+/*
+    public void chooseBattleType(String command) {
+        Request request = new Request();
+
+        System.out.println("1.Single player");
+        String command = request.getNewCommand();
+        System.out.println("2.Multi player");
+        if (command.equals("single player")) {
+            GameType gameType = GameType.SinglePlayer;
+            System.out.println("Single player mode!");
+            System.out.println("1.Story Mode!");
+            System.out.println("2.Custom Game!");
+            if (request.getNewCommand().equals("story mode")) {
+                System.out.println("you chose Story Mode!");
+
+            } else if (request.getNewCommand().toLowerCase().trim().equals("custom game")) {
+                System.out.println("you chose Custom Game!");
+            }
+        } else if (command.equals("multi player")) {
+            System.out.println("Multi player mode!!");
+            GameType gameType = GameType.MultiPlayer;
+            Account account2 = new Account();
+            if (account2.checkUserAndPass()) {
+                System.out.println("game is between " + Controller.currentAccount + " and " + account2.getUsername());
+                System.out.println("battle started!");
+            }
+            view.playerOptions();
+            int number = view.enteredNum();
+            if (number == 1) {
+                view.singlePlayerChosen();
+            } else if (number == 2) {
+                view.multiPlayerChosen();
+                Account account3 = new Account();
+                account2.setUserAndPass();
+                view.gameDeclaration(account2);
+            }
+        }
+    }
+*/
+
+    public void showMyMinions() {
+
+    }
+
+    public void showOpponentMinions() {
+
+    }
+
+    public void showCardInfo(String cardId) {
+
+    }
+
+    public void select(String cardId) {
+
+    }
+
+    public void moveTo(int x, int y) {
+
+    }
+
+
+    public void attack(Card myCard, Card opponentCard) {
+
+    }
+
+    public boolean attackComboPossible(String... opponentCardId) {
+        return true;
+
+    }
+
+    public void attackCombo(String opponentCardId, String myCardId1, String myCardId2, String... myCard) {
+
+    }
+
+    public void useSpecialPower(int x, int y) {
+
+    }
+
+    public void showHand() {
+
+    }
+
+    public void insert(String CardId, int x, int y) {
+
+    }
+
+    public void endTurn() {
+
+    }
+
+    public void help() {
+        view.showMinionsYouCanMove();
+        view.showMinionsYouCanAttack();
+        view.showMinionsYouCanAttack();
     }
 
 }
