0523bba5c3f92f7ecb6ec02d280e253587fd8253
danial <danialbarari007@gmail.com>
2019-05-05 22:00:54 +0430

    Ai

--- src/model/AI.java
+++ src/model/AI.java
@@ -1,20 +1,38 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class AI {
+public class AI extends Account{
+Game game = new Game();
     private Deck mainDdeck;
     private ArrayList<Card> AICards = new ArrayList<>();
    // private ArrayList<Deck> AIDecks;
-
-
-    public void move(Card card , Block blockmaghsad) {
+    public AI (){}
+    private void insertcard(){}
+    private void move() {
 
     }
-    public void attack(Card card , ArrayList<Block> blocks) {
+   private void attack() {
 
 
 
+    }
+    public void PlayGame(){
+        Random random = new Random();
+        int action =random.nextInt()%3;
+        switch (action){
+            case 0:
+                move();
+                break;
+            case 1:
+                attack();
+                break;
+            case 2 :
+
+                insertcard();
+                game.endTurn();
+        }
     }
 
     public ArrayList<Card> getAICards() {
