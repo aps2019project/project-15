1d4b05e341366a3add823f6a981996dccd91dbc5
sepehr <sepilami@ce.sharif.edu>
2019-06-08 10:38:19 +0430

    Graphical phase started

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-import controller.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import view.Graphic;
+import view.PersonOverview;
 
-public class Main {
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Graphic.class.getResource("Graphic.fxml"));
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.main();
+        launch(args);
     }
 }
