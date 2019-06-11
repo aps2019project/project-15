d651ec16776c4f50dd1600590bf3df939647a702
sepehr <sepilami@ce.sharif.edu>
2019-06-11 14:01:09 +0430

    Graphical phase started

---
+++ src/view/Graphic.java
@@ -0,0 +1,30 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class Graphic {
+    @FXML
+    public ImageView image;
+
+    public void exit(MouseEvent mouseEvent) {
+        System.out.println("you clicked exit");
+    }
+
+    public void help(MouseEvent mouseEvent) {
+        System.out.println("help");
+    }
+
+    public void shopMenufunc(MouseEvent mouseEvent) {
+        System.out.println("shop menu function");
+    }
+
+    public void collectionMenufunc(MouseEvent mouseEvent) {
+        System.out.println("collection menu function");
+    }
+
+    public void battleMenuFunc(MouseEvent mouseEvent) {
+        System.out.println("battle menu function");
+    }
+}
