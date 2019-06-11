8d4abc5b586b03d0ecbd9844b8d90e1105f2c212
sepehr <sepilami@ce.sharif.edu>
2019-06-11 14:21:00 +0430

    Graphical phase started

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import view.AccountMenu;
 import view.Graphic;
-import view.PersonOverview;
-
-import java.awt.*;
 
 public class Main extends Application {
 
