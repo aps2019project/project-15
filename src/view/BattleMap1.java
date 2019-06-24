a8e965f4020cba951cd39d6f8473461add8cd26a
sepehr <sepilami@ce.sharif.edu>
2019-06-24 14:49:56 +0430

    battle upgraded

--- src/view/BattleMap1.java
+++ src/view/BattleMap1.java
@@ -1,9 +1,19 @@
 package view;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class BattleMap1 {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BattleMap1 extends Application {
+
+    private int counter = 1;
 
     public Pane block00;
     public Pane block01;
@@ -18,5 +28,32 @@ public class BattleMap1 {
     }
 
     public void endTurn(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        ImageView imageView = new ImageView();
+        Image image = new Image("/unit_gifs/boss_andromeda_breathing.gif");
+        imageView.setImage(image);
+
+        Timer timer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                imageView.setLayoutX(counter);
+                imageView.setLayoutY(100);
+                System.out.println(counter);
+                counter++;
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 1, 100);
+        root.getChildren().addAll(imageView);
+
+        Scene scene = new Scene(root, 1600, 900);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
