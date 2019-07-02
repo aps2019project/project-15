999cd51541ca97baeb87e98c03c754ec31928b27
sepehr <sepilami@ce.sharif.edu>
2019-07-02 15:55:10 +0430

    phase 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import com.gilecode.yagson.YaGson;
 import com.gilecode.yagson.YaGsonBuilder;
 import controller.Controller;
@@ -9,15 +8,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Account;
 import model.DataCenter;
+import server.SocketPair;
 import view.UI;
 
-import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main extends Application {
 
+    static ArrayList<SocketPair> sockets = new ArrayList<>();
     static Controller controller = new Controller();
     private int counter = 1;
 
@@ -28,20 +33,24 @@ public class Main extends Application {
         Parent accountMenu = FXMLLoader.load(view.AccountMenu.class.getResource("AccountMenu.fxml"));
         primaryStage.setTitle("DUELYST");
         primaryStage.setScene(new Scene(accountMenu, 3000, 1000));
-
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         try {
+            try {
+                networking();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             loadAccounts();
             Controller.getInstance().initEverything();
             launch(args);
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
         } finally {
-
             Controller.currentAccount.setLoggedIn(false);
         }
-
     }
 
     private static void loadAccounts() {
@@ -60,6 +69,58 @@ public class Main extends Application {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void networking() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8989);
+
+        new Thread() {
+            Scanner scanner = new Scanner(System.in);
+
+            @Override
+            public void run() {
+                if (scanner.nextLine().equals("poweroff")) {
+                    System.exit(0);
+                }
+            }
+        }.start();
+
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+
+                new Thread(() -> {
+                    System.out.println("socket added");
+                    try {
+                        sockets.add(new SocketPair(socket));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Scanner scanner = null;
+                    try {
+                        scanner = new Scanner(socket.getInputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    while (socket.isConnected()) {
+                        if (scanner != null && scanner.hasNextLine()) {
+                            String message = scanner.nextLine();
+                            for (SocketPair other : sockets) {
+                                if (other.getSocket().equals(socket)) continue;
+                                synchronized (other) {
+                                    other.getFormatter().format(message + "\n");
+                                    other.getFormatter().flush();
+                                }
+                            }
+                            System.out.println(message);
+                        }
+                    }
+                }).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ src/server/Main.java
@@ -0,0 +1,54 @@
+package server;
+
+import client.Message;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket(InetAddress.getLocalHost(), 8989);
+            OutputStream outputStream = socket.getOutputStream();
+            Formatter formatter = new Formatter(outputStream);
+
+            System.out.println("enter your name:");
+            String clientName = scanner.nextLine();
+
+            new Thread(() -> {
+                String message = scanner.nextLine();
+                while (!message.equals("poweroff") && socket.isConnected()) {
+                    formatter.format(new Message(clientName, message).toJson() + "\n");
+                    formatter.flush();
+                    message = scanner.nextLine();
+                }
+                try {
+                    socket.close();
+                } catch (IOException ignored) {
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    Scanner scanner = new Scanner(socket.getInputStream());
+
+                    while (true) {
+                        if (scanner.hasNextLine()) {
+                            System.out.print(Message.fromJson(scanner.nextLine()));
+                        }
+                    }
+                } catch (IOException ignored) {
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
